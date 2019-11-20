f7db3f5ba3539db6fd89136acd4cfe51d6903ce2
Bruno de Almeida Honorato <bruno.almeidah@gmail.com>
2019-11-19 23:47:34 -0300

    Cria manutenção de estudantes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,15 @@
 import { Router } from 'express';
+import authMiddleware from './app/middleware/auth';
 import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StudentController';
 
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.post('/students', StudentController.store);
+routes.put('/students/:id', StudentController.update);
 
 export default routes;
