e2707b30640532af33af287839f4c28a10a051a5
Bruno de Almeida Honorato <bruno.almeidah@gmail.com>
2019-11-22 09:36:50 -0300

    Cria gestão de planos

---
+++ backend/src/app/controllers/PlanController.js
@@ -0,0 +1,59 @@
+import * as Yup from 'yup';
+import Plan from '../models/Plan';
+
+class PlanController {
+  async index(req, res) {
+    const plans = await Plan.findAll();
+    return res.json(plans);
+  }
+
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      duration: Yup.number()
+        .min(1)
+        .required(),
+      price: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'validation fails' });
+    }
+
+    const plan = await Plan.create(req.body);
+
+    return res.json(plan);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string(),
+      duration: Yup.number().min(1),
+      price: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'validation fails' });
+    }
+
+    const plan = await Plan.findByPk(req.params.id);
+
+    if (!plan) {
+      return res.status(400).json({ error: 'Plan not found' });
+    }
+
+    const planUpdated = await plan.update(req.body);
+
+    return res.json(planUpdated);
+  }
+
+  async delete(req, res) {
+    const plan = await Plan.findByPk(req.params.id);
+
+    await plan.destroy();
+
+    return res.json({ message: 'Plan successfully deleted' });
+  }
+}
+
+export default new PlanController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import authMiddleware from './app/middleware/auth';
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
+import PlanController from './app/controllers/PlanController';
 
 const routes = new Router();
 
@@ -11,5 +12,10 @@ routes.use(authMiddleware);
 
 routes.post('/students', StudentController.store);
 routes.put('/students/:id', StudentController.update);
+
+routes.post('/plans', PlanController.store);
+routes.get('/plans', PlanController.index);
+routes.put('/plans/:id', PlanController.update);
+routes.delete('/plans/:id', PlanController.delete);
 
 export default routes;
